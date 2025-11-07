2a31aba415874713e61e630b1ac69d1806bcebbc
Xaynerbb <zainablazeez22@gmail.com>
2025-11-07 23:16:27 +0100

    Java Programs

---
+++ Xayynn.java
@@ -0,0 +1,16 @@
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Xayynn {
+    public static void main(String[] args) {
+        // System.out.println("Xaynerbb is a good gurlll");
+        int number = 100;
+        System.out.println(number);
+        Date date = new Date();
+        long theLong = 78_339_578_028_475L;
+        System.out.println(theLong);
+        LocalDate now = LocalDate.now();
+        System.out.println(now.getMonth());
+    }
+}
